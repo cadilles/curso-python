8284401e3a11819f37f1cd6af61b2565f673cb2e
cadilles <cadilles@hotmail.com>
2019-11-17 18:47:06 -0300

    add files

---
+++ 4521-python/HandsOn/code/config.py
@@ -0,0 +1,20 @@
+from pymongo import MongoClient
+
+mongo_con = MongoClient()
+mongo_db = mongo_con['flask-app']
+
+
+
+""" inserted = mongo_db.user.insert_one(
+     {
+         "name": "usuario_nome",
+         "email": "usuario_email",
+         "messages": [
+             {
+                 "name": "usuario_nome",
+                 "message": "mensagem"
+             }
+         ]
+     }
+ )
+print(inserted) """
---
+++ 4521-python/HandsOn/code/usuarios/blueprint.py
@@ -0,0 +1,83 @@
+from flask import Flask, Blueprint, request, Response
+from config import mongo_db
+from bson.json_util import dumps
+
+
+usuarios_routes = Blueprint(
+    "usuarios",
+    __name__,
+    url_prefix="/usuarios"
+)
+
+
+
+@usuarios_routes.route("", methods=["DELETE"])
+def deleteUsuario():
+    try:
+        user = request.get_json()
+        deleted = mongo_db.user.delete_one(
+            { "email": user['email'] }            
+        )
+        if deleted.deleted_count:
+            response = {"message": "Usuário '%s'  removido com sucesso! "%(user["email"]) }
+            return Response(dumps(response), status=200, content_type="application/json")
+        else:
+            response = {"message": "Usuário '%s'  não encontrado. "%(user["email"]) }
+            return Response(dumps(response), status=404, content_type="application/json")
+
+    except Exception as e:
+        return "Erro: %s"%(e)
+
+
+
+@usuarios_routes.route("", methods=["PATCH"])
+def updateUsuario():
+    try:
+        user = request.get_json()
+        updated = mongo_db.user.update_one(
+            { "email": user['email'] },
+            { "$set": user }
+        )
+        if updated.modified_count:
+            response = {"message": "Usuário '%s'  atualizado com sucesso! "%(user["name"]) }
+            return Response(dumps(response), status=200, content_type="application/json")
+        elif updated.matched_count:
+            response = {"message": "Usuário '%s'  encontrado, mas não foi modificado. "%(user["name"]) }
+            return Response(dumps(response), status=400, content_type="application/json")
+        else:
+            response = {"message": "Usuário '%s'  não encontrado. "%(user["name"]) }
+            return Response(dumps(response), status=404, content_type="application/json")
+
+    except Exception as e:
+        return "Erro: %s"%(e)
+
+
+@usuarios_routes.route("")
+def getUsuarios():
+    try:
+        users = mongo_db.user.find()
+        return Response(dumps(users), status=200, content_type="application/json")
+
+    except Exception as e:
+        return "Erro: %s"%(e)
+
+
+@usuarios_routes.route("", methods=["POST"])
+def postUsuarios():
+    try:
+        user = request.get_json()
+        mongo_db.user.insert_one(
+            {
+                "name" : user["name"],
+                "email" : user["email"],
+                "messages" : []
+            }
+        )
+        response = {
+            "message": "Usuário '%s' criado com sucesso!" %  (user["name"])
+        }
+        return Response(dumps(response), status=201, content_type="application/json")
+    except Exception as e:
+        return "Erro: %s " % (e)
+        
+
